8cf73dec0329fb0c76f91cd518a06df6f9b3edeb
tajmahak <tajmahal@bk.ru>
2019-05-15 23:27:07 +1000

    Расчёт расстояния Левенштейна

--- MyLibrary/Data/Format.cs
+++ MyLibrary/Data/Format.cs
@@ -314,5 +314,65 @@ namespace MyLibrary.Data
                 throw new NotImplementedException();
             }
         }
+
+        /// <summary>
+        /// Процентное соотношение совпадений двух строк на основе расчёта расстояния Левенштейна.
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static decimal LevenshteinDistancePercent(string value1, string value2)
+        {
+            var distance = (decimal)LevenshteinDistance(value1, value2);
+            var maxLength = (decimal)Math.Max(value1.Length, value2.Length);
+            var percent = 100m - (distance / maxLength * 100m);
+            return percent;
+        }
+        /// <summary>
+        /// Расчёт расстояния Левенштейна между двумя строками 
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static int LevenshteinDistance(string value1, string value2)
+        {
+            int n = value1.Length;
+            int m = value2.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+
+            for (int j = 0; j <= m; d[0, j] = j++) ;
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (value2[j - 1] == value1[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
     }
 }
