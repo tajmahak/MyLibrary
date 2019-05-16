d5514284352ca10d143f3684d99ceb0af1a66c48
tajmahak <tajmahal@bk.ru>
2019-05-16 16:50:08 +1000

    clipboardMonitor

--- MyLibrary/NativeMethods.cs
+++ MyLibrary/NativeMethods.cs
@@ -7,5 +7,11 @@ namespace MyLibrary
     {
         [DllImport("user32.dll")]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("User32.dll")]
+        internal static extern int SetClipboardViewer(int hWndNewViewer);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        internal static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
     }
 }
