2c0b69f66d1de05a6e0ab3e49af2f3547ce225ed
tajmahak <tajmahal@bk.ru>
2019-05-16 21:05:56 +1000

    разработка...

---
+++ MyLibrary/Interop/ShellThumbnail.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using static MyLibrary.NativeMethods;
+
+namespace MyLibrary.Interop
+{
+    public class ShellThumbnail : IDisposable
+    {
+        public static Icon GetFileIcon(string path, bool smallSize, bool linkOverlay = false)
+        {
+            var shfi = new Shfileinfo();
+            var flags = ShgfiIcon | ShgfiUsefileattributes;
+            if (linkOverlay)
+            {
+                flags += ShgfiLinkoverlay;
+            }
+
+            /* Check the size specified for return. */
+            if (smallSize)
+            {
+                flags += ShgfiSmallicon;
+            }
+            else
+            {
+                flags += ShgfiLargeicon;
+            }
+            SHGetFileInfo(path,
+                FileAttributeNormal,
+                ref shfi,
+                (uint)Marshal.SizeOf(shfi),
+                flags);
+
+            // Copy (clone) the returned icon to a new object, thus allowing us to clean-up properly
+            var icon = (Icon)Icon.FromHandle(shfi.hIcon).Clone();
+            DestroyIcon(shfi.hIcon); // Cleanup
+            return icon;
+        }
+
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                if (_alloc != null)
+                {
+                    Marshal.ReleaseComObject(_alloc);
+                }
+                _alloc = null;
+                if (ThumbNail != null)
+                {
+                    ThumbNail.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+        ~ShellThumbnail()
+        {
+            Dispose();
+        }
+
+
+        public Bitmap ThumbNail { get; private set; }
+        public Size DesiredSize { get; set; } = new Size(100, 100);
+        public Bitmap GetThumbnail(string fileName)
+        {
+            if (ThumbNail != null)
+            {
+                ThumbNail.Dispose();
+                ThumbNail = null;
+            }
+            IShellFolder folder = null;
+            try
+            {
+                folder = GetDesktopFolder;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            if (folder != null)
+            {
+                IntPtr pidlMain = IntPtr.Zero;
+                try
+                {
+                    int cParsed = 0;
+                    int pdwAttrib = 0;
+                    string filePath = Path.GetDirectoryName(fileName);
+                    folder.ParseDisplayName(IntPtr.Zero, IntPtr.Zero, filePath, ref cParsed, ref pidlMain, ref pdwAttrib);
+                }
+                catch (Exception ex)
+                {
+                    Marshal.ReleaseComObject(folder);
+                    throw ex;
+                }
+                if (pidlMain != IntPtr.Zero)
+                {
+                    Guid iidShellFolder = new Guid("000214E6-0000-0000-C000-000000000046");
+                    IShellFolder item = null;
+                    try
+                    {
+                        folder.BindToObject(pidlMain, IntPtr.Zero, ref iidShellFolder, ref item);
+                    }
+                    catch (Exception ex)
+                    {
+                        Marshal.ReleaseComObject(folder);
+                        Allocator.Free(pidlMain);
+                        throw ex;
+                    }
+                    if (item != null)
+                    {
+                        IEnumIDList idEnum = null;
+                        try
+                        {
+                            item.EnumObjects(IntPtr.Zero, (ESHCONTF.SHCONTF_FOLDERS | ESHCONTF.SHCONTF_NONFOLDERS), ref idEnum);
+                        }
+                        catch (Exception ex)
+                        {
+                            Marshal.ReleaseComObject(folder);
+                            Allocator.Free(pidlMain);
+                            throw ex;
+                        }
+                        if (idEnum != null)
+                        {
+                            int hRes = 0;
+                            IntPtr pidl = IntPtr.Zero;
+                            int fetched = 0;
+                            bool complete = false;
+                            while (!complete)
+                            {
+                                hRes = idEnum.Next(1, ref pidl, ref fetched);
+                                if (hRes != 0)
+                                {
+                                    pidl = IntPtr.Zero;
+                                    complete = true;
+                                }
+                                else
+                                {
+                                    if (GetThumbNail(fileName, pidl, item))
+                                    {
+                                        complete = true;
+                                    }
+                                }
+                                if (pidl != IntPtr.Zero)
+                                {
+                                    Allocator.Free(pidl);
+                                }
+                            }
+                            Marshal.ReleaseComObject(idEnum);
+                        }
+                        Marshal.ReleaseComObject(item);
+                    }
+                    Allocator.Free(pidlMain);
+                }
+                Marshal.ReleaseComObject(folder);
+            }
+            return ThumbNail;
+        }
+
+
+
+
+
+        private IMalloc Allocator
+        {
+            get
+            {
+                if (!_disposed)
+                {
+                    if (_alloc == null)
+                    {
+                        SHGetMalloc(ref _alloc);
+                    }
+                }
+                else
+                {
+                    Debug.Assert(false, "Object has been disposed.");
+                }
+                return _alloc;
+            }
+        }
+        private bool GetThumbNail(string file, IntPtr pidl, IShellFolder item)
+        {
+            IntPtr hBmp = IntPtr.Zero;
+            IExtractImage extractImage = null;
+            try
+            {
+                string pidlPath = PathFromPidl(pidl);
+                if (Path.GetFileName(pidlPath).ToUpper().Equals(Path.GetFileName(file).ToUpper()))
+                {
+                    IUnknown iunk = null;
+                    int prgf = 0;
+                    Guid iidExtractImage = new Guid("BB2E617C-0920-11d1-9A0B-00C04FC2D6C1");
+                    item.GetUIObjectOf(IntPtr.Zero, 1, ref pidl, ref iidExtractImage, ref prgf, ref iunk);
+                    extractImage = (IExtractImage)iunk;
+                    if (extractImage != null)
+                    {
+                        SIZE sz = new SIZE
+                        {
+                            cx = DesiredSize.Width,
+                            cy = DesiredSize.Height
+                        };
+                        StringBuilder location = new StringBuilder(260, 260);
+                        int priority = 0;
+                        int requestedColourDepth = 32;
+                        EIEIFLAG flags = EIEIFLAG.IEIFLAG_ASPECT | EIEIFLAG.IEIFLAG_SCREEN;
+                        int uFlags = (int)flags;
+                        try
+                        {
+                            extractImage.GetLocation(location, location.Capacity, ref priority, ref sz, requestedColourDepth, ref uFlags);
+                            extractImage.Extract(ref hBmp);
+                        }
+                        catch (COMException)
+                        {
+
+                        }
+                        if (hBmp != IntPtr.Zero)
+                        {
+                            ThumbNail = Image.FromHbitmap(hBmp);
+                        }
+                        Marshal.ReleaseComObject(extractImage);
+                        extractImage = null;
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (hBmp != IntPtr.Zero)
+                {
+                    DeleteObject(hBmp);
+                }
+                if (extractImage != null)
+                {
+                    Marshal.ReleaseComObject(extractImage);
+                }
+                throw ex;
+            }
+        }
+        private string PathFromPidl(IntPtr pidl)
+        {
+            StringBuilder path = new StringBuilder(260, 260);
+            int result = SHGetPathFromIDList(pidl, path);
+            if (result == 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return path.ToString();
+            }
+        }
+        private IShellFolder GetDesktopFolder
+        {
+            get
+            {
+                IShellFolder ppshf = null;
+                int r = SHGetDesktopFolder(ref ppshf);
+                return ppshf;
+            }
+        }
+        private IMalloc _alloc = null;
+        private bool _disposed = false;
+
+    }
+}
--- MyLibrary/NativeMethods.cs
+++ MyLibrary/NativeMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace MyLibrary
 {
@@ -10,12 +11,43 @@ namespace MyLibrary
 
         [DllImport("user32.dll")]
         internal static extern int SetClipboardViewer(int hWndNewViewer);
+
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         internal static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         internal static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
-        [DllImport("User32.dll")]
+
+        [DllImport("user32.dll")]
         internal static extern int DestroyIcon(IntPtr hIcon);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SHGetMalloc(ref IMalloc ppMalloc);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SHGetDesktopFolder(ref IShellFolder ppshf);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SHGetPathFromIDList(IntPtr pidl, StringBuilder pszPath);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto)]
+        internal static extern int DeleteObject(IntPtr hObject);
+
+        [DllImport("shell32.dll")]
+        internal static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref Shfileinfo psfi, uint cbFileInfo, uint uFlags);
+
+        #region const
+
+        internal const uint ShgfiIcon = 0x000000100;     // get icon
+        internal const uint ShgfiLinkoverlay = 0x000008000;     // put a link overlay on icon
+        internal const uint ShgfiLargeicon = 0x000000000;     // get large icon
+        internal const uint ShgfiSmallicon = 0x000000001;     // get small icon
+        internal const uint ShgfiUsefileattributes = 0x000000010;     // use passed dwFileAttribute
+        internal const uint FileAttributeNormal = 0x00000080;
+
+        #endregion
+
+        #region struct
 
         [StructLayout(LayoutKind.Sequential)]
         internal struct Shfileinfo
@@ -30,19 +62,211 @@ namespace MyLibrary
             private readonly string szTypeName;
         };
 
-        internal const uint ShgfiIcon = 0x000000100;     // get icon
-        internal const uint ShgfiLinkoverlay = 0x000008000;     // put a link overlay on icon
-        internal const uint ShgfiLargeicon = 0x000000000;     // get large icon
-        internal const uint ShgfiSmallicon = 0x000000001;     // get small icon
-        internal const uint ShgfiUsefileattributes = 0x000000010;     // use passed dwFileAttribute
-        internal const uint FileAttributeNormal = 0x00000080;
+        [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 0, CharSet = CharSet.Auto)]
+        internal struct STRRET_CSTR
+        {
+            public ESTRRET uType;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 520)]
+            public byte[] cStr;
+        }
 
-        [DllImport("shell32.dll")]
-        internal static extern IntPtr SHGetFileInfo(
-            string pszPath,
-            uint dwFileAttributes,
-            ref Shfileinfo psfi,
-            uint cbFileInfo,
-            uint uFlags);
+        [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Auto)]
+        internal struct STRRET_ANY
+        {
+            [FieldOffset(0)]
+            public ESTRRET uType;
+            [FieldOffset(4)]
+            public IntPtr pOLEString;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SIZE
+        {
+            public int cx;
+            public int cy;
+        }
+
+        #endregion
+
+        #region interface
+
+        [ComImport]
+        [Guid("00000002-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IMalloc
+        {
+
+            [PreserveSig()]
+            IntPtr Alloc(int cb);
+
+            [PreserveSig()]
+            IntPtr Realloc(IntPtr pv, int cb);
+
+            [PreserveSig()]
+            void Free(IntPtr pv);
+
+            [PreserveSig()]
+            int GetSize(IntPtr pv);
+
+            [PreserveSig()]
+            int DidAlloc(IntPtr pv);
+
+            [PreserveSig()]
+            void HeapMinimize();
+        }
+
+        [ComImport]
+        [Guid("000214F2-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IEnumIDList
+        {
+
+            [PreserveSig()]
+            int Next(int celt, ref IntPtr rgelt, ref int pceltFetched);
+
+            void Skip(int celt);
+
+            void Reset();
+
+            void Clone(ref IEnumIDList ppenum);
+        }
+
+        [ComImport]
+        [Guid("000214E6-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IShellFolder
+        {
+
+            void ParseDisplayName(IntPtr hwndOwner, IntPtr pbcReserved,
+              [MarshalAs(UnmanagedType.LPWStr)]string lpszDisplayName,
+              ref int pchEaten, ref IntPtr ppidl, ref int pdwAttributes);
+
+            void EnumObjects(IntPtr hwndOwner,
+              [MarshalAs(UnmanagedType.U4)]ESHCONTF grfFlags,
+              ref IEnumIDList ppenumIDList);
+
+            void BindToObject(IntPtr pidl, IntPtr pbcReserved, ref Guid riid,
+              ref IShellFolder ppvOut);
+
+            void BindToStorage(IntPtr pidl, IntPtr pbcReserved, ref Guid riid, IntPtr ppvObj);
+
+            [PreserveSig()]
+            int CompareIDs(IntPtr lParam, IntPtr pidl1, IntPtr pidl2);
+
+            void CreateViewObject(IntPtr hwndOwner, ref Guid riid,
+              IntPtr ppvOut);
+
+            void GetAttributesOf(int cidl, IntPtr apidl,
+              [MarshalAs(UnmanagedType.U4)]ref ESFGAO rgfInOut);
+
+            void GetUIObjectOf(IntPtr hwndOwner, int cidl, ref IntPtr apidl, ref Guid riid, ref int prgfInOut, ref IUnknown ppvOut);
+
+            void GetDisplayNameOf(IntPtr pidl,
+              [MarshalAs(UnmanagedType.U4)]ESHGDN uFlags,
+              ref STRRET_CSTR lpName);
+
+            void SetNameOf(IntPtr hwndOwner, IntPtr pidl,
+              [MarshalAs(UnmanagedType.LPWStr)]string lpszName,
+              [MarshalAs(UnmanagedType.U4)] ESHCONTF uFlags,
+              ref IntPtr ppidlOut);
+        }
+
+        [ComImport]
+        [Guid("BB2E617C-0920-11d1-9A0B-00C04FC2D6C1")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IExtractImage
+        {
+            void GetLocation([Out(), MarshalAs(UnmanagedType.LPWStr)]
+        StringBuilder pszPathBuffer, int cch, ref int pdwPriority, ref SIZE prgSize, int dwRecClrDepth, ref int pdwFlags);
+
+            void Extract(ref IntPtr phBmpThumbnail);
+        }
+
+        [ComImport]
+        [Guid("00000000-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IUnknown
+        {
+
+            [PreserveSig()]
+            IntPtr QueryInterface(ref Guid riid, ref IntPtr pVoid);
+
+            [PreserveSig()]
+            IntPtr AddRef();
+
+            [PreserveSig()]
+            IntPtr Release();
+        }
+
+        #endregion
+
+        #region enum
+
+        [Flags]
+        internal enum ESTRRET
+        {
+            STRRET_WSTR = 0,
+            STRRET_OFFSET = 1,
+            STRRET_CSTR = 2
+        }
+
+        [Flags]
+        internal enum ESHCONTF
+        {
+            SHCONTF_FOLDERS = 32,
+            SHCONTF_NONFOLDERS = 64,
+            SHCONTF_INCLUDEHIDDEN = 128,
+        }
+
+        [Flags]
+        internal enum ESHGDN
+        {
+            SHGDN_NORMAL = 0,
+            SHGDN_INFOLDER = 1,
+            SHGDN_FORADDRESSBAR = 16384,
+            SHGDN_FORPARSING = 32768
+        }
+
+        [Flags]
+        internal enum ESFGAO
+        {
+            SFGAO_CANCOPY = 1,
+            SFGAO_CANMOVE = 2,
+            SFGAO_CANLINK = 4,
+            SFGAO_CANRENAME = 16,
+            SFGAO_CANDELETE = 32,
+            SFGAO_HASPROPSHEET = 64,
+            SFGAO_DROPTARGET = 256,
+            SFGAO_CAPABILITYMASK = 375,
+            SFGAO_LINK = 65536,
+            SFGAO_SHARE = 131072,
+            SFGAO_READONLY = 262144,
+            SFGAO_GHOSTED = 524288,
+            SFGAO_DISPLAYATTRMASK = 983040,
+            SFGAO_FILESYSANCESTOR = 268435456,
+            SFGAO_FOLDER = 536870912,
+            SFGAO_FILESYSTEM = 1073741824,
+            SFGAO_HASSUBFOLDER = -2147483648,
+            SFGAO_CONTENTSMASK = -2147483648,
+            SFGAO_VALIDATE = 16777216,
+            SFGAO_REMOVABLE = 33554432,
+            SFGAO_COMPRESSED = 67108864,
+        }
+
+        internal enum EIEIFLAG
+        {
+            IEIFLAG_ASYNC = 1,
+            IEIFLAG_CACHE = 2,
+            IEIFLAG_ASPECT = 4,
+            IEIFLAG_OFFLINE = 8,
+            IEIFLAG_GLEAM = 16,
+            IEIFLAG_SCREEN = 32,
+            IEIFLAG_ORIGSIZE = 64,
+            IEIFLAG_NOSTAMP = 128,
+            IEIFLAG_NOBORDER = 256,
+            IEIFLAG_QUALITY = 512
+        }
+
+        #endregion
     }
 }
