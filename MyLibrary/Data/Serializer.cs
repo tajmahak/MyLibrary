d6723768208f3522af9532185153ca9d1641bdbc
tajmahak <tajmahal@bk.ru>
2019-08-24 22:15:35 +1000

    сериализация в текстовом формате

--- MyLibrary/Data/Serializer.cs
+++ MyLibrary/Data/Serializer.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace MyLibrary.Data
 {
     public static class Serializer
     {
-        public static void InitializeType(Type type)
+        public static void InitializeXmlType(Type type)
         {
             lock (_xmlSerializers)
             {
@@ -38,10 +39,61 @@ namespace MyLibrary.Data
             }
         }
 
+        public static string SerializeToText(object obj)
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties();
+
+            var str = new StringBuilder();
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(obj, null);
+                str.AppendLine($"{property.Name} = {value}");
+            }
+            return str.ToString();
+        }
+        public static T DeserializeFromText<T>(string text)
+        {
+            var config = Activator.CreateInstance<T>();
+
+            var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var properties = typeof(T).GetProperties();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                line = line.Trim();
+
+                if (line.Length == 0 || line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                var charIndex = line.IndexOf('=');
+                var parameterName = line.Substring(0, charIndex).TrimEnd();
+                var parameterValue = line.Remove(0, charIndex + 1).TrimStart();
+
+                parameterName = parameterName.ToUpperInvariant();
+                var property = Array.Find(properties, x => x.Name.ToUpper() == parameterName);
+                if (property != null)
+                {
+                    try
+                    {
+                        var value = Convert.ChangeType(parameterValue, property.PropertyType);
+                        property.SetValue(config, value, null);
+                    }
+                    catch
+                    {
+                        throw new Exception($"Неверный формат значения для параметра конфигурации '{property.Name}'");
+                    }
+                }
+            }
+            return config;
+        }
+
         private static readonly Dictionary<Type, XmlSerializer> _xmlSerializers = new Dictionary<Type, XmlSerializer>();
         private static XmlSerializer GetXmlSerializer(Type type)
         {
-            InitializeType(type);
+            InitializeXmlType(type);
             lock (_xmlSerializers)
             {
                 return _xmlSerializers[type];
