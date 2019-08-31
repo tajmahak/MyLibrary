5c887d5129c075b8444c3edacd6b9af4c916b620
tajmahak <tajmahal@bk.ru>
2019-08-31 11:12:39 +1000

    +коллекция

---
+++ MyLibrary/DataBase/DBQueryStructureBlockCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLibrary.DataBase
+{
+    public class DBQueryStructureBlockCollection : List<DBQueryStructureBlock>
+    {
+        public void Add(DBQueryStructureType type, params object[] args)
+        {
+            Add(new DBQueryStructureBlock
+            {
+                Type = type,
+                Args = args,
+            });
+        }
+        public List<DBQueryStructureBlock> FindAll(Predicate<DBQueryStructureType> predicate)
+        {
+            return FindAll(x => predicate(x.Type));
+        }
+        public List<DBQueryStructureBlock> FindAll(DBQueryStructureType type)
+        {
+            return FindAll(x => x == type);
+        }
+        public List<DBQueryStructureBlock> FindAll(Predicate<string> predicate)
+        {
+            return FindAll(x => predicate(x.Type.ToString()));
+        }
+        public DBQueryStructureBlock Find(Predicate<DBQueryStructureType> type)
+        {
+            return Find(x => type(x.Type));
+        }
+        public DBQueryStructureBlock Find(DBQueryStructureType type)
+        {
+            return Find(x => x == type);
+        }
+    }
+}
