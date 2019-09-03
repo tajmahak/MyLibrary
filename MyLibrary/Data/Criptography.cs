7db4b172103bf053c8b1dba09add2326fd5af6f9
tajmahak <tajmahal@bk.ru>
2019-09-03 22:50:20 +1000

    поддержка алгоритма AES

--- MyLibrary/Data/Criptography.cs
+++ MyLibrary/Data/Criptography.cs
@@ -1,58 +1,72 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 
 namespace MyLibrary.Data
 {
     public static class Cryptography
     {
-        public static byte[] EncryptAES(byte[] data, byte[] key, byte[] iv)
+        public static byte[] EncryptAES(byte[] data, byte[] key)
         {
             using (var aes = Aes.Create())
             {
-                aes.KeySize = 128;
+                aes.KeySize = key.Length * 8;
                 aes.BlockSize = 128;
-                aes.Padding = PaddingMode.Zeros;
+                aes.Padding = PaddingMode.PKCS7;
 
                 aes.Key = key;
-                aes.IV = iv;
+                aes.GenerateIV();
 
+                using (var ms = new MemoryStream())
+                using (var writer = new BinaryWriter(ms))
                 using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
                 {
-                    return PerformCryptography(data, encryptor);
+                    writer.Write(data.Length);
+                    writer.Write(aes.IV);
+                    writer.Write(PerformCryptography(data, encryptor));
+                    return ms.ToArray();
                 }
             }
         }
-
-        public static byte[] DecryptAES(byte[] data, byte[] key, byte[] iv)
+        public static byte[] DecryptAES(byte[] data, byte[] key)
         {
             using (var aes = Aes.Create())
             {
-                aes.KeySize = 128;
+                aes.KeySize = key.Length * 8;
                 aes.BlockSize = 128;
                 aes.Padding = PaddingMode.Zeros;
 
-                aes.Key = key;
-                aes.IV = iv;
-
-                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream(data))
+                using (var reader = new BinaryReader(ms))
                 {
-                    return PerformCryptography(data, decryptor);
+                    var dataLength = reader.ReadInt32();
+
+                    aes.Key = key;
+                    aes.IV = reader.ReadBytes(aes.IV.Length);
+
+                    using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                    {
+                        var decryptData = reader.ReadBytes((int)(ms.Length - ms.Position));
+                        decryptData = PerformCryptography(decryptData, decryptor);
+
+                        if (decryptData.Length != dataLength)
+                        {
+                            Array.Resize(ref decryptData, dataLength);
+                        }
+
+                        return decryptData;
+                    }
                 }
             }
+
+
+
+
+
+
+
+
         }
-
-        private static byte[] PerformCryptography(byte[] data, ICryptoTransform cryptoTransform)
-        {
-            using (var ms = new MemoryStream())
-            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
-            {
-                cryptoStream.Write(data, 0, data.Length);
-                cryptoStream.FlushFinalBlock();
-
-                return ms.ToArray();
-            }
-        }
-
         public static byte[] GetRandomBytes(int length, bool nonZero = false)
         {
             var data = new byte[length];
@@ -68,6 +82,17 @@ namespace MyLibrary.Data
                 }
             }
             return data;
+        }
+
+        private static byte[] PerformCryptography(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using (var ms = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+                return ms.ToArray();
+            }
         }
     }
 }
