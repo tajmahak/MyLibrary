e5577a238e4a7c32d8d256747850c9b2ec5ff28f
tajmahak <tajmahal@bk.ru>
2019-09-03 22:26:15 +1000

    сброс до оригинала

--- MyLibrary/Data/Criptography.cs
+++ MyLibrary/Data/Criptography.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Security.Cryptography;
 
-namespace MyLibrary.MyLibrary.Data
+namespace MyLibrary.Data
 {
     public static class Cryptography
     {
@@ -10,44 +9,65 @@ namespace MyLibrary.MyLibrary.Data
         {
             using (var aes = Aes.Create())
             {
-                aes.KeySize = key.Length * 8;
-                aes.BlockSize = iv.Length * 8;
+                aes.KeySize = 128;
+                aes.BlockSize = 128;
                 aes.Padding = PaddingMode.Zeros;
 
                 aes.Key = key;
                 aes.IV = iv;
 
-                using (var ms = new MemoryStream())
                 using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
-                using (var cryptoStream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    cryptoStream.Write(data, 0, data.Length);
-                    cryptoStream.FlushFinalBlock();
-                    return ms.ToArray();
+                    return PerformCryptography(data, encryptor);
                 }
             }
         }
+
         public static byte[] DecryptAES(byte[] data, byte[] key, byte[] iv)
         {
             using (var aes = Aes.Create())
             {
-                aes.KeySize = key.Length * 8;
-                aes.BlockSize = iv.Length * 8;
+                aes.KeySize = 128;
+                aes.BlockSize = 128;
                 aes.Padding = PaddingMode.Zeros;
 
                 aes.Key = key;
                 aes.IV = iv;
 
-                using (var ms = new MemoryStream())
                 using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
-                using (var cryptoStream = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
                 {
-                    cryptoStream.Write(data, 0, data.Length);
-                    cryptoStream.FlushFinalBlock();
-                    return ms.ToArray();
+                    return PerformCryptography(data, decryptor);
                 }
             }
         }
 
+        private static byte[] PerformCryptography(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using (var ms = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] GetRandomBytes(int length, bool nonZero = false)
+        {
+            var data = new byte[length];
+            using (var provider = RNGCryptoServiceProvider.Create())
+            {
+                if (nonZero)
+                {
+                    provider.GetNonZeroBytes(data);
+                }
+                else
+                {
+                    provider.GetBytes(data);
+                }
+            }
+            return data;
+        }
     }
 }
